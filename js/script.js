85fe5a729e50d31e5dba97a40aeff930d4205494
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-10 14:40:57 +0200

    create script to generate password

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Ciao");
+const userFirstName = prompt('Qual è il tuo nome? Per favore scrivilo senza spazi');
+const userLastName = prompt('Qual è il tuo cognome? Per favore scrivilo senza spazi');
+const userBestColor = prompt('Qual è il tuo colore preferito?');
+const year = '22';
+const passwordGenerator = userFirstName + userLastName + userBestColor + year;
+
+const output = `
+La tua password è "${passwordGenerator}"
+`;
+
+document.getElementById('password').innerHTML = output;
+
