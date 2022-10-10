79afa42c3d0766a661936519702d32d65091a1ee
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-10 14:55:04 +0200

    change const year from string to number

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const userFirstName = prompt('Qual è il tuo nome? Per favore scrivilo senza spazi');
-const userLastName = prompt('Qual è il tuo cognome? Per favore scrivilo senza spazi');
-const userBestColor = prompt('Qual è il tuo colore preferito?');
-const year = '22';
+const userFirstName = prompt('Qual è il tuo nome? Per favore scrivilo senza spazi.');
+const userLastName = prompt('Qual è il tuo cognome? Per favore scrivilo senza spazi.');
+const userBestColor = prompt('Qual è il tuo colore preferito? Per favore scrivilo senza spazi.');
+const year = 22;
 const passwordGenerator = userFirstName + userLastName + userBestColor + year;
 
 const output = `
